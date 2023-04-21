1fa09d9f29768b045988fdd82984b78da9fec2d0
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-04-21 16:56:17 +0300

    NavBar seen

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,13 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  // console.log(projects);
+
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div key={projects.id} project={projects}><ProjectItem /></div>
     </div>
   );
 }
